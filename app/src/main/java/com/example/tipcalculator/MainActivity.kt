d6510c878a9059034ec00c74a81ae5aa40021f9f
kolanse <samuel.ochuba@decagon.dev>
2020-08-08 23:42:09 +0100

    done with the app logic

--- app/src/main/java/com/example/tipcalculator/MainActivity.kt
+++ app/src/main/java/com/example/tipcalculator/MainActivity.kt
@@ -2,10 +2,25 @@ package com.example.tipcalculator
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        /**
+         * sample way of calling the class, first parameter is the number of people, second parameter is the percentage tip,
+         * third parameter is the bill amount
+         *
+         */
+
+        var getTip = tip(5, 10, 5000)
+
+
+        var billPerPerson = getTip.getPerPerson() // method getPerPerson() is in charge of getting the value per person.
+
+        Log.d("Ans", "$billPerPerson") // log the output. You can delete this code later
+
     }
 }
---
+++ app/src/main/java/com/example/tipcalculator/tip.kt
@@ -0,0 +1,18 @@
+package com.example.tipcalculator
+
+class tip(numberOfPeople: Int, tipPercentage: Int, billAmount: Int){
+
+    var numberOfPeople: Double = numberOfPeople.toDouble()
+    var tipPercentage: Double = tipPercentage.toDouble()
+    var billAmount: Double = billAmount.toDouble()
+
+    fun getPerPerson():Double{
+
+        var ans = ((tipPercentage * 100) + billAmount) / numberOfPeople
+
+
+
+        return ans
+    }
+
+}
